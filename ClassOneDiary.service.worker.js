13be2d2baaa8a6c0d5c9c52f126ce905e642d264
Lenovo <sukabulit@user.noreply.gitee.com>
2025-10-03 16:43:07 +0800

    修复了名字问题

--- ClassOneDiary.service.worker.js
+++ ClassOneDiary.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759470876|1948207601';
+const CACHE_VERSION = '1759480955|12027509079';
 /** @type {string} */
 const CACHE_PREFIX = '日志Class One-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
