3c94dd1bc3e4d1acdbec49ddf6f2e8a9b9fd30a5
Lenovo <sukabulit@user.noreply.gitee.com>
2025-10-07 13:39:31 +0800

    修复bug

--- ClassOneDiary.service.worker.js
+++ ClassOneDiary.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759632688|6190077971';
+const CACHE_VERSION = '1759813959|5852512597';
 /** @type {string} */
 const CACHE_PREFIX = '日志Class One-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
