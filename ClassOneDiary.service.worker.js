091ecd435e19beadc4ee051cacb17ad6cee5f22a
Lenovo <sukabulit@user.noreply.gitee.com>
2025-10-18 19:00:08 +0800

    开发者

--- ClassOneDiary.service.worker.js
+++ ClassOneDiary.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759927435|27989866371';
+const CACHE_VERSION = '1760785181|16741707843';
 /** @type {string} */
 const CACHE_PREFIX = '日志Class One-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
