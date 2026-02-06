abe0c148af305794f09d09da121fb543426f80cd
bupig <13146553+sukabulit@user.noreply.gitee.com>
2026-02-06 21:19:05 +0800

    取消调试

--- ClassOneDiary.service.worker.js
+++ ClassOneDiary.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770382673|17742110936';
+const CACHE_VERSION = '1770383904|18972985059';
 /** @type {string} */
 const CACHE_PREFIX = '日志Class One-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
