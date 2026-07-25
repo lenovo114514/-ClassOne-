5c28c6c0a4327e3f6f7504d1857a7b184ca86d72
bupig <13146553+sukabulit@user.noreply.gitee.com>
2026-07-25 22:25:42 +0800

    Add project files with LFS

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770792775|2393171010';
+const CACHE_VERSION = '1784986967|98281830254';
 /** @type {string} */
 const CACHE_PREFIX = '日志Class One-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
