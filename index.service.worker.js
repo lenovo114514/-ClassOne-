9473e2b9728fac6413a4afc4ec5459346af44702
bupig <13146553+sukabulit@user.noreply.gitee.com>
2026-08-20 17:02:51 +0800

    正式

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786795935|168245725651';
+const CACHE_VERSION = '1787216547|588861390441';
 /** @type {string} */
 const CACHE_PREFIX = '日志Class One-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
